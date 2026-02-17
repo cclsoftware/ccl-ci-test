d20b3954e06c372a985c8c8f14309ef1f8b9677e
Julian Wolff <jwolff@presonus.com>
2026-02-17 18:04:52 +0100

    prepare windows signing

--- .github/ci/win/prepare_signing.sh
+++ .github/ci/win/prepare_signing.sh
@@ -5,7 +5,7 @@ scriptdir=$(dirname "$0")
 signfile="${scriptdir}/../../../build/signing/ccl/signing.cmake"
 
 echo "set (SIGNING_AZURE_KEYVAULT_WIN \"${{ secrets.SIGNING_AZURE_KEYVAULT_WIN }}\")" >> ${signfile}
-set (SIGNING_AZURE_CLIENTID_WIN \"${{ secrets.SIGNING_AZURE_CLIENTID_WIN }}\")
-set (SIGNING_AZURE_TENANTID_WIN \"${{ secrets.SIGNING_AZURE_TENANTID_WIN }}\")
-set (SIGNING_CERTIFICATE_WIN \"${{ secrets.SIGNING_CERTIFICATE_WIN }}\")
-set (SIGNING_TIMESTAMP_SERVER_WIN "${SIGNING_TIMESTAMP_SERVER_WIN}")
+echo "set (SIGNING_AZURE_CLIENTID_WIN \"${{ secrets.SIGNING_AZURE_CLIENTID_WIN }}\")" >> ${signfile}
+echo "set (SIGNING_AZURE_TENANTID_WIN \"${{ secrets.SIGNING_AZURE_TENANTID_WIN }}\")" >> ${signfile}
+echo "set (SIGNING_CERTIFICATE_WIN \"${{ secrets.SIGNING_CERTIFICATE_WIN }}\")" >> ${signfile}
+echo "set (SIGNING_TIMESTAMP_SERVER_WIN "${SIGNING_TIMESTAMP_SERVER_WIN}")" >> ${signfile}
