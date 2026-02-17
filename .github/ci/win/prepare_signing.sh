a07d0d2df5d1112ddd1470f472c6b71cc9323818
Julian Wolff <jwolff@presonus.com>
2026-02-17 18:11:17 +0100

    prepare windows signing

--- .github/ci/win/prepare_signing.sh
+++ .github/ci/win/prepare_signing.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 scriptdir=$(dirname "$0")
 signfile="${scriptdir}/../../../build/signing/ccl/signing.cmake"
 
-echo "set (SIGNING_AZURE_KEYVAULT_WIN \"${{ secrets.SIGNING_AZURE_KEYVAULT_WIN }}\")" >> ${signfile}
-echo "set (SIGNING_AZURE_CLIENTID_WIN \"${{ secrets.SIGNING_AZURE_CLIENTID_WIN }}\")" >> ${signfile}
-echo "set (SIGNING_AZURE_TENANTID_WIN \"${{ secrets.SIGNING_AZURE_TENANTID_WIN }}\")" >> ${signfile}
-echo "set (SIGNING_CERTIFICATE_WIN \"${{ secrets.SIGNING_CERTIFICATE_WIN }}\")" >> ${signfile}
-echo "set (SIGNING_TIMESTAMP_SERVER_WIN "${SIGNING_TIMESTAMP_SERVER_WIN}")" >> ${signfile}
+echo 'set (SIGNING_AZURE_KEYVAULT_WIN "'"${{ secrets.SIGNING_AZURE_KEYVAULT_WIN }}"'")' >> "${signfile}"
+echo 'set (SIGNING_AZURE_CLIENTID_WIN "'"${{ secrets.SIGNING_AZURE_CLIENTID_WIN }}"'")' >> "${signfile}"
+echo 'set (SIGNING_AZURE_TENANTID_WIN "'"${{ secrets.SIGNING_AZURE_TENANTID_WIN }}"'")' >> "${signfile}"
+echo 'set (SIGNING_CERTIFICATE_WIN "'"${{ secrets.SIGNING_CERTIFICATE_WIN }}"'")' >> "${signfile}"
+echo 'set (SIGNING_TIMESTAMP_SERVER_WIN "'"${SIGNING_TIMESTAMP_SERVER_WIN}"'")' >> "${signfile}"
